acfd8c104ec130b2b8e65b7ff370f2217e5b090a
Gabriella Moura <138831365+GaMoura01@users.noreply.github.com>
2023-07-07 11:16:11 -0300

    Add files via upload
    Ex 2 | Computational Logical Using Python
    
    Exercício feito na aula 1 da matéria Logica Computacional Utilizando Python com o professor Fernando Leonid.

---
+++ exercicio2.py
@@ -0,0 +1,10 @@
+#Crie um programa que solicite o nome, a idade e a cidade 
+# onde a pessoa mora, e retorne uma frase utilizando essas três informações.
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+cidade = input("Digite sua cidade: ")
+
+mensagem = f"Olá {nome}, voce tem {idade} anos e é de {cidade}"
+
+print(mensagem)
