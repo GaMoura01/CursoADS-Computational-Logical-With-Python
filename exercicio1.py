dbb4d4b86165b19646873a62057f31b8c1005657
Gabriella Moura <138831365+GaMoura01@users.noreply.github.com>
2023-07-07 11:12:16 -0300

    Add files via upload
    Ex 1 - Computational Logical Using Python

---
+++ exercicio1.py
@@ -0,0 +1,7 @@
+# Crie um programa que solicite um nome e retorne uma saudação utilizando o nome fornecido.
+
+nome = input("Digite seu nome: ")
+
+retorno = f"Olá {nome}, seja bem vindo"
+
+print(retorno)
